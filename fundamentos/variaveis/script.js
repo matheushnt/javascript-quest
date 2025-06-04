882de023668a4ffda637b84daa69960c7e520e08
Matheus Henrique <matheushnt06@gmail.com>
2025-06-04 20:32:46 -0300

    Variáveis

---
+++ fundamentos/variaveis/script.js
@@ -0,0 +1,36 @@
+var nome = 'Matheus';
+var idade = 20;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+
+console.log(totalPreco);
+
+var sobrenome = 'Nunes',
+  cidade = 'Fortaleza';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir);
+
+var comida;
+console.log(comida);
+var comida = 'Macarronada';
+console.log(comida);
+
+let time = 'Vasco';
+console.log(time);
+time = 'Palmeiras';
+console.log(time);
+
+var frontEnd;
+var backEnd;
+var fullStack;
+var inteligenciaArtificial;
+var programador;
