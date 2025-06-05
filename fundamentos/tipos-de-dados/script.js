3718599035fcc63849989a7e1eee563f24391536
Matheus Henrique <matheushnt06@gmail.com>
2025-06-05 19:47:37 -0300

    Tipos de dados

---
+++ fundamentos/tipos-de-dados/script.js
@@ -0,0 +1,24 @@
+var time = 'Palmeiras';
+var titulosBrasileirao = 11;
+console.log(typeof titulosBrasileirao);
+
+var nome = 'Matheus';
+var sobrenome = 'Nunes';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+var gols = 18;
+var frase = 'Romário fez ' + gols + 'gols';
+console.log(typeof frase);
+
+var ano = '2025';
+var mes = 6;
+console.log(ano + mes);
+
+var temporada;
+var frase2 = "O 'melhor' jogador da Premier League";
+console.log(frase2);
+
+var titulosCopaDoMundo = 5;
+var titulosBrasil = `O Brasil possui ${titulosCopaDoMundo} títulos de Copa do Mundo`;
+console.log(titulosBrasil);
