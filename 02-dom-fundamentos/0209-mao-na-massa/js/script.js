9f11e180a9af3b66717f475e7be9f69200684679
Matheus Henrique <matheushnt06@gmail.com>
2025-06-22 17:16:20 -0300

    Animação ao Scroll

--- 02-dom-fundamentos/0209-mao-na-massa/js/script.js
+++ 02-dom-fundamentos/0209-mao-na-massa/js/script.js
@@ -70,3 +70,24 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+    function animaScroll() {
+      sections.forEach((item) => {
+        const sectionTop = item.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) item.classList.add('ativo');
+        else item.classList.remove('ativo');
+      });
+    }
+
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+
+initAnimacaoScroll();
