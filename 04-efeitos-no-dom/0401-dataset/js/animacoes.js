04e2f0911d5911068f1db90936e9878693921169
Matheus Henrique <matheushnt06@gmail.com>
2025-07-02 17:23:23 -0300

    Dataset 2

--- 04-efeitos-no-dom/0401-dataset/js/animacoes.js
+++ 04-efeitos-no-dom/0401-dataset/js/animacoes.js
@@ -1,13 +1,14 @@
 function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
   if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add('ativo');
 
     function activeTab(index) {
       tabContent.forEach((item) => item.classList.remove('ativo'));
-      tabContent[index].classList.add('ativo');
+      const direcao = tabContent[index].dataset.anime;
+      tabContent[index].classList.add('ativo', direcao);
     }
 
     tabMenu.forEach((item, index) => {
@@ -21,7 +22,7 @@ function initTabNav() {
 initTabNav();
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
   const activeClass = 'ativo';
 
   if (accordionList.length) {
@@ -42,7 +43,7 @@ function initAccordion() {
 initAccordion();
 
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
 
   function scrollToSection(event) {
     event.preventDefault();
@@ -72,7 +73,7 @@ function initScrollSuave() {
 initScrollSuave();
 
 function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
 
   if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
