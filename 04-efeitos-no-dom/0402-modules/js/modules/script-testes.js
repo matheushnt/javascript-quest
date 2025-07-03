66f16d71ca0a3a07fa07a857aae9594488d7ce9c
Matheus Henrique <matheushnt06@gmail.com>
2025-07-03 18:48:35 -0300

    Modules 2

---
+++ 04-efeitos-no-dom/0402-modules/js/modules/script-testes.js
@@ -0,0 +1,9 @@
+export function testar() {
+  console.log('Isso é um teste');
+}
+
+export function novoTeste() {
+  console.log('Isso é um novo teste');
+}
+
+export const senha = 'x0b0Sw^#zx(v';
--- 04-efeitos-no-dom/0402-modules/js/script.js
+++ 04-efeitos-no-dom/0402-modules/js/script.js
@@ -2,6 +2,11 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/animacao-scroll.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import * as testes from './modules/script-testes.js';
+
+testes.testar();
+testes.novoTeste();
+console.log(testes.senha);
 
 initScrollSuave();
 initAnimacaoScroll();
