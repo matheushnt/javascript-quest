ac44a2abadfcb64979d9e81e8a9e6c48804ef844
Matheus Henrique <matheushnt06@gmail.com>
2025-07-06 17:44:32 -0300

    Dropdown Menu

--- 04-efeitos-no-dom/0404-tooltip/0402-modules/js/modules/script-testes.js
+++
@@ -1,9 +0,0 @@
-export function testar() {
-  console.log('Isso é um teste');
-}
-
-export function novoTeste() {
-  console.log('Isso é um novo teste');
-}
-
-export const senha = 'x0b0Sw^#zx(v';
--- 04-efeitos-no-dom/0405-dropdown-menu/js/script.js
+++ 04-efeitos-no-dom/0405-dropdown-menu/js/script.js
@@ -2,13 +2,14 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/animacao-scroll.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
-import * as testes from './modules/script-testes.js';
-
-testes.testar();
-testes.novoTeste();
-console.log(testes.senha);
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
