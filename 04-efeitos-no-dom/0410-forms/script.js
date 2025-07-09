42ff56b82f1b0173c5b09a1d7dce542b1163b970
Matheus Henrique <matheushnt06@gmail.com>
2025-07-09 15:38:26 -0300

    Forms 2

--- 04-efeitos-no-dom/0410-forms/script.js
+++ 04-efeitos-no-dom/0410-forms/script.js
@@ -8,9 +8,20 @@ console.log(form[0].email); // Retorno igual a linha de cima
 const formContato = document.forms[0];
 const spanTexto = document.querySelector('.texto');
 
-function handleKeyUp(event) {
-  console.log(event.target.value);
-  spanTexto.innerText = event.target.value;
+function handleChange(event) {
+  const target = event.target;
+
+  if (!target.checkValidity()) {
+    target.classList.add('invalido');
+    formContato.nome.setCustomValidity('O campo Nome é totalmente necessário');
+    target.nextElementSibling.innerText = target.validationMessage;
+  }
+
+  // document.body.style.backgroundColor = target.value; input color
+
+  if (target.checked) {
+    console.log(target.value);
+  }
 }
 
-formContato.addEventListener('change', handleKeyUp);
+formContato.addEventListener('change', handleChange);
