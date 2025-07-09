b9e1fb0d1ae6fba901a490b67f6b8378f5a0bc40
Matheus Henrique <matheushnt06@gmail.com>
2025-07-09 15:46:27 -0300

    Forms 3

--- 04-efeitos-no-dom/0410-forms/script.js
+++ 04-efeitos-no-dom/0410-forms/script.js
@@ -6,22 +6,11 @@ console.log(form[0][1]);
 console.log(form[0].email); // Retorno igual a linha de cima
 
 const formContato = document.forms[0];
-const spanTexto = document.querySelector('.texto');
+const dados = {};
 
 function handleChange(event) {
-  const target = event.target;
-
-  if (!target.checkValidity()) {
-    target.classList.add('invalido');
-    formContato.nome.setCustomValidity('O campo Nome é totalmente necessário');
-    target.nextElementSibling.innerText = target.validationMessage;
-  }
-
-  // document.body.style.backgroundColor = target.value; input color
-
-  if (target.checked) {
-    console.log(target.value);
-  }
+  dados[event.target.name] = event.target.value;
+  console.log(dados);
 }
 
 formContato.addEventListener('change', handleChange);
