6759e5a263e21e0b5c443b582efb83f9eb1e6faa
Matheus Henrique <matheushnt06@gmail.com>
2025-07-30 10:35:28 -0300

    Refatora a função fetchBitCoin

--- 10-refatoracao-projeto-final/js/modules/fetch-bitcoin.js
+++ 10-refatoracao-projeto-final/js/modules/fetch-bitcoin.js
@@ -1,8 +1,8 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then(res => res.json())
     .then(json => {
-      const spanBitcoin = document.querySelector('.bitcoin-valor');
+      const spanBitcoin = document.querySelector(target);
       spanBitcoin.innerText = (1000 / json.BRL.buy).toFixed(5);
     })
     .catch(erro => console.log(Error(erro)));
